54b9fe14a228f0f18baebb72374919fe4ed06832
Dmitry Orlov <me@mosquito.su>
2017-12-07 22:34:25 +0300

    [fix] add python 3.6

--- pidfile/__init__.py
+++ pidfile/__init__.py
@@ -4,7 +4,7 @@ author_info = (
     ("Dmitry Orlov", "me@mosquito.su"),
 )
 
-version_info = (1, 0, 2)
+version_info = (1, 0, 3)
 
 __version__ = ".".join(map(str, version_info))
 __author__ = ", ".join("{0} <{1}>".format(*author) for author in author_info)
